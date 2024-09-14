8791be2486cce6856f3218fc38734791f387ec02
Chanchreek <145703811+Chanchreek@users.noreply.github.com>
2024-09-14 18:14:37 +0530

    Update mySolution.sh

--- mySolution.sh
+++ mySolution.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Download the tarball
 wget https://exit-zero-academy.github.io/DevOpsTheHardWayAssets/linux_project/secretGenerator.tar.gz
 
-# Extract the tarball
 tar -xvf secretGenerator.tar.gz
 
-# Navigate into the src directory
 cd src
 
-# Prepare the environment
 mkdir -p secretDir
 rm -rf maliciousFiles
 touch secretDir/.secret
@@ -19,5 +15,4 @@ if [ -L 'important.link' ] && [ ! -e 'important.link' ]; then
 fi
 echo "your content here" > CONTENT_TO_HASH
 
-# Run the script to generate the secret
 /bin/bash generateSecret.sh
